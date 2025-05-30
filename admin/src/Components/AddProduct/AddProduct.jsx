28e68b83387579c12ac57860f3567c5e377ca5bb
Wobo Goodness <119409678+goody09083729531@users.noreply.github.com>
2025-05-30 01:54:19 +0100

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -29,7 +29,7 @@ const AddProduct = () => {
         let formData = new FormData();
         formData.append('product', image);
 
-        await fetch('https://novabackend-1byq.onrender.com/upload', {
+        await fetch('https://novabackend-xaio.onrender.com/upload', {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
@@ -41,7 +41,7 @@ const AddProduct = () => {
         {
             product.image = responseData.image_url;
             console.log(product);
-            await fetch('https://novabackend-1byq.onrender.com/addproduct', {
+            await fetch('https://novabackend-xaio.onrender.com/addproduct', {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
